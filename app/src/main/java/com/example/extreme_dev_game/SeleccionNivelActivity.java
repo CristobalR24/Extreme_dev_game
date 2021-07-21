59b615707c791628d6d017ab8241a507b4c05504
Cristobal_Rodriguez <cristobalr2400@gmail.com>
2021-07-21 00:10:48 -0500

    Estilo seleccionnivel

--- app/src/main/java/com/example/extreme_dev_game/SeleccionNivelActivity.java
+++ app/src/main/java/com/example/extreme_dev_game/SeleccionNivelActivity.java
@@ -3,6 +3,7 @@ package com.example.extreme_dev_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SeleccionNivelActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class SeleccionNivelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_nivel);
+    }
+
+    public void RegresarMenu(View view) {
+        finish();
     }
 }
